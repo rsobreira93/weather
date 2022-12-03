84c0a3b6cc21d54f4c5c6989349aaceb3630c976
rsobreira93 <sobreiraromuloes@gmail.com>
2022-12-03 07:55:16 -0300

    refactor: update error on user controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,10 +1,10 @@
 import { Controller, Post } from "@overnightjs/core";
 import { User } from "@src/models/user";
 import { Request, Response } from "express";
-import mongoose from "mongoose";
+import { BaseController } from ".";
 
 @Controller("users")
-export class UsersController {
+export class UsersController extends BaseController {
   @Post("")
   public async create(request: Request, response: Response): Promise<void> {
     try {
@@ -14,11 +14,7 @@ export class UsersController {
 
       response.status(201).send(userCreated);
     } catch (error: any) {
-      if (error instanceof mongoose.Error.ValidationError) {
-        response.status(400).send({ error: error.message });
-      } else {
-        response.status(500).send({ error: "Internal Server Error" });
-      }
+      this.sendCreateUpdateErrorResponse(response, error);
     }
   }
 }
