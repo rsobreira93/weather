6febb2c24ecba761ffed6a77aee3f4f1285dfe82
rsobreira93 <sobreiraromuloes@gmail.com>
2022-12-09 09:04:38 -0300

    feat: add greceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,46 @@
 import { SetupServer } from "./server";
 import config from "../config/default.json";
+import logger from "./logger";
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error(
+    `App exiting due to an unexpected promise: ${promise} and reason: ${reason}`
+  );
+  throw reason;
+});
+
+process.on("uncaughtException", (error) => {
+  logger.error(`App exiting due to an uncaught unexpected ${error}`);
+  process.exit(ExitStatus.Failure);
+});
 
 (async (): Promise<void> => {
-  const server = new SetupServer(config.App.port);
-  await server.init();
-  server.start();
+  try {
+    const server = new SetupServer(config.App.port);
+    await server.init();
+    server.start();
+
+    const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
+
+    exitSignals.map((signal) =>
+      process.on(signal, async () => {
+        try {
+          await server.close();
+          logger.info(`App exited with status`);
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          logger.error(`App exited with error: ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
+  } catch (error) {
+    logger.error(`App exited with error: ${error}`);
+    process.exit(ExitStatus.Failure);
+  }
 })();
