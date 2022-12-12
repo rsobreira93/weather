725f878e08ce9edb0c27a374eb9011438041dd18
rsobreira93 <sobreiraromuloes@gmail.com>
2022-12-12 07:41:38 -0300

    test: adjust test to rating 2

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -55,8 +55,8 @@ describe("Beach forecast functional tests", () => {
     const { body, status } = await global.testRequest
       .get("/forecast")
       .set({ "x-access-token": token });
+
     expect(status).toBe(200);
-    // Make sure we use toEqual to check value not the object and array itself
     expect(body).toEqual(apiForecastResponse1BeachFixture);
   });
 
